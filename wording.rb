acb251b650756b44a03f322285ff510bca8a70b9
Terry Raimondo <terry.raimondo@gmail.com>
2016-03-01 10:29:01 +0100

    Rewording

--- wording.rb
+++ wording.rb
@@ -1,19 +1,5 @@
 # coding: utf-8
 class Wording
-  URGENT = [
-    'Poliiiice !',
-    'Salut je m\'appelle Terry et j\'ai pas envie de te répondre',
-    'Merci de laisser un message après le biosdkvalsfgndsfljgnkldjfsnglsdnfglnsdjlfgn',
-    'Kikoo, sava?',
-    'Regarde derrière toi',
-    'Macdo pls',
-    'Chaque intervention vous coûte 390€',
-    'Non merci, cordialement',
-    'Je dors pas, je médite',
-    'סְתוֹם אֵת פִּיךָ',
-    'Non'
-  ]
-
   COOL = [
     'GRAVE ! COOL. Aller, macdo',
     'Coolcool',
@@ -24,21 +10,12 @@ class Wording
 
   OBEY = [
     'Écrivez moi ce dont vous avez besoin, je vous expliquerai comment vous en passer',
-    'Je suis une vraie personne, aimez-moi.',
     'Conçu avec amour le 10/02/2016',
-    'Bonsoir, je suis votre maître.',
-    'En cas d\'urgence technique ou de pause clope, tapez "wake up idiot"',
     'Au travail maintenant',
     'C\'est qui le papa ?',
-    'Le canal #tech est très intérressant ! Jetez-y un oeil',
+    'Le canal #tech est très intéressant ! Jetez-y un oeil',
     'Vous pouvez mute les notifications spécifiques à un canal',
     'Je ne suis qu\'amour',
-    '"wake up idiot", c\'est chiant. Faites le avec modération',
-    'Solitude',
-    'Je m\'appelle Bastien',
-    'Je m\'appelle Romain',
     'Pour avoir une liste des commandes disponibles, tapez "j\'adore le foie gras et les papillons morts"',
-    'Je t\'aime',
-    'Je suis gros et moche'
   ]
 end
